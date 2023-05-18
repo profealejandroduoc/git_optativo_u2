050d2dea3cc9fb2e9e00b34f5b6cf321d4ea924c
El profe <al.sepulvedam@profesor.duoc.cl>
2023-05-18 17:39:22 -0400

    modelo listo

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    rut=models.CharField(primary_key=True,max_length=10)
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
