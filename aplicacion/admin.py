1b9fbf74379d49bbe8bf7134517c19c3400df705
El profe <al.sepulvedam@profesor.duoc.cl>
2023-05-18 17:44:40 -0400

    Modelo ok

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Persona
 
 # Register your models here.
+class admPersona(admin.ModelAdmin):
+    list_display=["rut","nombre","apellido"]
+    list_editable=["nombre","apellido"]
+
+    class meta:
+        model=Persona
+
+admin.site.register(Persona,admPersona)
