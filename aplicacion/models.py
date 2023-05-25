12edd37c33b8ddc1d73244e9231089e126a06070
El profe <al.sepulvedam@profesor.duoc.cl>
2023-05-25 17:00:55 -0400

    Nuevo release

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -5,6 +5,7 @@ class Persona(models.Model):
     rut=models.CharField(primary_key=True,max_length=10)
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=150, default="por ahi 123")
 
 
     def __str__(self):
